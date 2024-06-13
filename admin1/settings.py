49440d3d448e725dc5d2eaa9219dc03ab04d5d78
SRIDHAR-08 <sridhar200288@gmail.com>
2024-06-13 17:19:57 +0530

    fgdh

--- admin1/settings.py
+++ admin1/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'admin1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-import environ
-
-env = environ.Env()
-environ.Env.read_env()
-
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# import environ
+
+# env = environ.Env()
+# environ.Env.read_env()
+
+# import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.parse(env('DATABASE_URL'))
+# }
 
 
 
